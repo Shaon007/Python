50538443051c83832ed6c015c403791c4fea7ba1
Md Nazir Hossain <146012511+Shaon007@users.noreply.github.com>
2024-04-05 19:39:20 +0600

    Create file_read.py

---
+++ file_read.py
@@ -0,0 +1,4 @@
+employee_file = open("C:\Python\shaon.txt",'r')
+print(employee_file.read())            #boolean response to check wheather the file has access
+#print(employee_file.readlines()[1])   #for individual line print
+employee_file.close()
