d72fdf3711757a2331756c8e6f1b87d2f5d7078d
Md Nazir Hossain <146012511+Shaon007@users.noreply.github.com>
2024-04-08 01:40:30 +0600

    Create CF-282A.py

---
+++ CF-282A.py
@@ -0,0 +1,15 @@
+def main():
+    n = int(input())
+    x = 0
+
+    for _ in range(n):
+        statement = input()
+        if "++" in statement:
+            x += 1
+        elif "--" in statement:
+            x -= 1
+
+    print(x)
+
+if __name__ == "__main__":
+    main()
