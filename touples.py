7b6ea530643b97c40d0cb5115c41ecb4fe9fbf8f
Md Nazir Hossain <146012511+Shaon007@users.noreply.github.com>
2024-10-30 16:13:56 +0600

    Update touples.py

--- touples.py
+++ touples.py
@@ -1,9 +1,9 @@
-coordinates = (3, 5)    #fixed values cant change () means touples; [] means list
+coordinates = (3, 5)    
 print(coordinates)  
 print(coordinates[0])
-coordinates1 = [(1,2),(2,3),(4,5)]  #tuples inside list, list item can be changed,but not touples 
+coordinates1 = [(1,2),(2,3),(4,5)] 
 print(coordinates1)
 coordinates1[2]= (4,2)
 print(coordinates1)
-coordinates[1] = 0      #illegal , not supported
+coordinates[1] = 0     
 
